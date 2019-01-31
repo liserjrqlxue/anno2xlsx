a8f4aba12d73fb9f66f6813f0a8ec39cefa0f52d
王垚燊(Yaoshen Wang) <wangyaoshen@genomics.cn>
2019-01-31 14:13:30 +0800

    split anno.go to tier.go and simple.go

--- anno/tier.go
+++ anno/tier.go
@@ -6,7 +6,6 @@ import (
 	"regexp"
 	//"github.com/liserjrqlxue/acmg2015"
 	"strconv"
-	"strings"
 )
 
 // Tier1 >1
@@ -31,134 +30,15 @@ var FuncInfo = map[string]int{
 	"splice+20":    1,
 }
 
-var long2short = map[string]string{
-	"Pathogenic":             "P",
-	"Likely Pathogenic":      "LP",
-	"Uncertain Significance": "VUS",
-	"Likely Benign":          "LB",
-	"Benign":                 "B",
-	"P":                      "P",
-	"LP":                     "LP",
-	"VUS":                    "VUS",
-	"LB":                     "LB",
-	"B":                      "B",
-}
-
 // regexp
 var (
 	isHgmd    = regexp.MustCompile("DM")
 	isClinvar = regexp.MustCompile("Pathogenic|Likely_pathogenic")
-	indexReg  = regexp.MustCompile(`\d+\.\s+`)
 	//newlineReg = regexp.MustCompile(`\n+`)
 	isDenovo  = regexp.MustCompile(`NA;NA$`)
 	noProband = regexp.MustCompile(`^NA`)
 	isChrAXY  = regexp.MustCompile(`[0-9XY]+$`)
 )
-
-func UpdateSnv(dataHash map[string]string) {
-
-	// pHGVS= pHGVS1+"|"+pHGVS3
-	if dataHash["pHGVS1"] != "" && dataHash["pHGVS3"] != "" {
-		dataHash["pHGVS"] = dataHash["pHGVS1"] + " | " + dataHash["pHGVS3"]
-	}
-
-	score, err := strconv.ParseFloat(dataHash["dbscSNV_ADA_SCORE"], 32)
-	if err != nil {
-		dataHash["dbscSNV_ADA_pred"] = dataHash["dbscSNV_ADA_SCORE"]
-	} else {
-		if score >= 0.6 {
-			dataHash["dbscSNV_ADA_pred"] = "D"
-		} else {
-			dataHash["dbscSNV_ADA_pred"] = "P"
-		}
-	}
-	score, err = strconv.ParseFloat(dataHash["dbscSNV_RF_SCORE"], 32)
-	if err != nil {
-		dataHash["dbscSNV_RF_pred"] = dataHash["dbscSNV_RF_SCORE"]
-	} else {
-		if score >= 0.6 {
-			dataHash["dbscSNV_RF_pred"] = "D"
-		} else {
-			dataHash["dbscSNV_RF_pred"] = "P"
-		}
-	}
-
-	score, err = strconv.ParseFloat(dataHash["GERP++_RS"], 32)
-	if err != nil {
-		dataHash["GERP++_RS_pred"] = dataHash["GERP++_RS"]
-	} else {
-		if score >= 2 {
-			dataHash["GERP++_RS_pred"] = "D"
-		} else {
-			dataHash["GERP++_RS_pred"] = "P"
-		}
-	}
-
-	// 0-0.6 不保守  0.6-2.5 保守 ＞2.5 高度保守
-	score, err = strconv.ParseFloat(dataHash["PhyloP Vertebrates"], 32)
-	if err != nil {
-		dataHash["PhyloP Vertebrates Pred"] = dataHash["PhyloP Vertebrates"]
-	} else {
-		if score >= 2.5 {
-			dataHash["PhyloP Vertebrates Pred"] = "高度保守"
-		} else if score > 0.6 {
-			dataHash["PhyloP Vertebrates Pred"] = "保守"
-		} else {
-			dataHash["PhyloP Vertebrates Pred"] = "不保守"
-		}
-	}
-	score, err = strconv.ParseFloat(dataHash["PhyloP Placental Mammals"], 32)
-	if err != nil {
-		dataHash["PhyloP Placental Mammals Pred"] = dataHash["PhyloP Placental Mammals"]
-	} else {
-		if score >= 2.5 {
-			dataHash["PhyloP Placental Mammals Pred"] = "高度保守"
-		} else if score > 0.6 {
-			dataHash["PhyloP Placental Mammals Pred"] = "保守"
-		} else {
-			dataHash["PhyloP Placental Mammals Pred"] = "不保守"
-		}
-	}
-
-	dataHash["烈性突变"] = "否"
-	if FuncInfo[dataHash["Function"]] == 3 {
-		dataHash["烈性突变"] = "是"
-	}
-
-	dataHash["HGMDorClinvar"] = "否"
-	if isHgmd.MatchString(dataHash["HGMD Pred"]) {
-		dataHash["HGMDorClinvar"] = "是"
-	}
-	if isClinvar.MatchString(dataHash["ClinVar Significance"]) {
-		dataHash["HGMDorClinvar"] = "是"
-	}
-
-	dataHash["GnomAD homo"] = dataHash["GnomAD HomoAlt Count"]
-	dataHash["GnomAD hemi"] = dataHash["GnomAD HemiAlt Count"]
-	dataHash["纯合，半合"] = dataHash["GnomAD HomoAlt Count"] // + "|" + dataHash["GnomAD HemiAlt Count"]
-	if len(strings.Split(dataHash["MutationName"], ":")) > 1 {
-		dataHash["MutationNameLite"] = dataHash["Transcript"] + ":" + strings.Split(dataHash["MutationName"], ":")[1]
-	} else {
-		dataHash["MutationNameLite"] = dataHash["MutationName"]
-	}
-
-	//dataHash["突变频谱"] = geneDb[geneSymbol]
-
-	dataHash["历史样本检出个数"] = dataHash["sampleMut"] + "/" + dataHash["sampleAll"]
-
-	// remove index
-	for _, k := range [2]string{"GeneralizationEN", "GeneralizationCH"} {
-		sep := "\n\n"
-		keys := strings.Split(dataHash[k], sep)
-		for i := range keys {
-			keys[i] = indexReg.ReplaceAllLiteralString(keys[i], "")
-		}
-		dataHash[k] = strings.Join(keys, sep)
-	}
-
-	dataHash["自动化判断"] = long2short[dataHash["ACMG"]]
-	return
-}
 
 func AddTier(item map[string]string, stats map[string]int, geneList, specVarDb map[string]bool, isTrio bool) {
 	if isTrio {
