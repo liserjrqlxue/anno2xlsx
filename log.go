858729786acdea711c222ed3fcab2570f4ebfb8b
Wang Yaoshen <wangyaoshen@bgi.com>
2019-05-09 10:42:46 +0800

    split func out of main

---
+++ log.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"github.com/liserjrqlxue/simple-util"
+	"log"
+	"time"
+)
+
+func logTierStats(stats map[string]int) {
+	fmt.Printf("Total               Count : %7d\n", stats["Total"])
+	if stats["Total"] == 0 {
+		return
+	}
+	if *trio {
+		fmt.Printf("  noProband         Count : %7d\n", stats["noProband"])
+
+		fmt.Printf("Denovo              Hit   : %7d\n", stats["Denovo"])
+		fmt.Printf("  Denovo B/LB       Hit   : %7d\n", stats["Denovo B/LB"])
+		fmt.Printf("  Denovo Tier1      Hit   : %7d\n", stats["Denovo Tier1"])
+		fmt.Printf("  Denovo Tier2      Hit   : %7d\n", stats["Denovo Tier2"])
+	}
+
+	fmt.Printf("ACMG noB/LB         Hit   : %7d\n", stats["noB/LB"])
+	if *trio {
+		fmt.Printf("  +isDenovo         Hit   : %7d\n", stats["isDenovo noB/LB"])
+		fmt.Printf("    +isAF           Hit   : %7d\n", stats["Denovo AF"])
+		fmt.Printf("      +isGene       Hit   : %7d\n", stats["Denovo Gene"])
+		fmt.Printf("        +isFunction Hit   : %7d\tTier1\n", stats["Denovo Function"])
+		fmt.Printf("        +noFunction Hit   : %7d\n", stats["Denovo noFunction"])
+		fmt.Printf("      +noGene       Hit   : %7d\n", stats["Denovo noGene"])
+		fmt.Printf("    +noAF           Hit   : %7d\n", stats["Denovo noAF"])
+		fmt.Printf("  +noDenovo         Hit   : %7d\n", stats["noDenovo noB/LB"])
+		fmt.Printf("    +isAF           Hit   : %7d\n", stats["noDenovo AF"])
+		fmt.Printf("      +isGene       Hit   : %7d\n", stats["noDenovo Gene"])
+		fmt.Printf("        +isFunction Hit   : %7d\tTier1\n", stats["noDenovo Function"])
+		fmt.Printf("        +noFunction Hit   : %7d\n", stats["noDenovo noFunction"])
+		fmt.Printf("      +noGene       Hit   : %7d\n", stats["noDenovo noGene"])
+		fmt.Printf("    +noAF           Hit   : %7d\n", stats["noDenovo noAF"])
+	} else {
+		fmt.Printf("    +isAF           Hit   : %7d\n", stats["isAF"])
+		fmt.Printf("      +isGene       Hit   : %7d\n", stats["isGene"])
+		fmt.Printf("        +isFunction Hit   : %7d\tTier1\n", stats["isFunction"])
+		fmt.Printf("        +noFunction Hit   : %7d\n", stats["noFunction"])
+		fmt.Printf("      +noGene       Hit   : %7d\n", stats["noGene"])
+		fmt.Printf("    +noAF           Hit   : %7d\n", stats["noAF"])
+	}
+
+	fmt.Printf("HGMD/ClinVar        Hit   : %7d\n", stats["HGMD/ClinVar"])
+	fmt.Printf("  isAF              Hit   : %7d\n", stats["HGMD/ClinVar isAF"])
+	fmt.Printf("    noMT            Hit   : %7d\tTier1\n", stats["HGMD/ClinVar noMT T1"])
+	fmt.Printf("  noAF              Hit   : %7d\n", stats["HGMD/ClinVar noAF"])
+	fmt.Printf("    noMT            Hit   : %7d\tTier2\n", stats["HGMD/ClinVar noMT T2"])
+
+	fmt.Printf("SpecVar             Hit   : %7d\n", stats["SpecVar"])
+
+	fmt.Printf("Retain              Count : %7d\n", stats["Retain"])
+	fmt.Printf("  Tier1             Count : %7d\n", stats["Tier1"])
+	fmt.Printf("    遗传相符        Count : %7d\n", stats["遗传相符"])
+	fmt.Printf("  Tier2             Count : %7d\n", stats["Tier2"])
+	fmt.Printf("  Tier3             Count : %7d\n", stats["Tier3"])
+}
+
+func logTime(timeList []time.Time, step1, step2 int, message string) {
+	trim := 3*8 - 1
+	str := simple_util.FormatWidth(trim, message, ' ')
+	log.Printf("%s\ttook %7.3fs to run.\n", str, timeList[step2].Sub(timeList[step1]).Seconds())
+}
---
+++ sheet.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"github.com/liserjrqlxue/anno2xlsx/anno"
+	"github.com/liserjrqlxue/simple-util"
+	"github.com/tealeg/xlsx"
+	"log"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func addFamInfoSheet(excel *xlsx.File, sheetName string, sampleList []string) {
+	sheet, err := excel.AddSheet(sheetName)
+	simple_util.CheckErr(err)
+
+	sheet.AddRow().AddCell().SetString("SampleID")
+
+	for _, sample := range sampleList {
+		sheet.AddRow().AddCell().SetString(sample)
+	}
+}
+
+func addCnv2Sheet(sheet *xlsx.Sheet, paths []string, sampleMap map[string]bool, filterSize, filterGene bool) {
+	cnvDb, _ := simple_util.LongFiles2MapArray(paths, "\t", nil)
+
+	// title
+	var title []string
+	for _, cell := range sheet.Row(0).Cells {
+		title = append(title, cell.Value)
+	}
+
+	for _, item := range cnvDb {
+		sample := item["Sample"]
+		item["Primer"] = anno.CnvPrimer(item, sheet.Name)
+		if sampleMap[sample] {
+			gene := item["OMIM_Gene"]
+			updateDiseaseMultiGene(gene, item, geneDiseaseDbColumn, geneDiseaseDb)
+			item["OMIM"] = item["OMIM_Phenotype_ID"]
+			if filterGene && item["OMIM"] == "" {
+				continue
+			}
+			if filterSize {
+				length, err := strconv.ParseFloat(item["Len(Kb)"], 16)
+				if err != nil {
+					log.Printf(
+						"can not ParseFloat of Len(Kb)[%s] for Summary[%s]\n",
+						item["Len(Kb)"], item["Summary"],
+					)
+				} else if length < 1000 {
+					continue
+				}
+			}
+			row := sheet.AddRow()
+			for _, key := range title {
+				row.AddCell().SetString(item[key])
+			}
+		}
+	}
+}
+
+func addSmnResult(sheet *xlsx.Sheet, paths []string, sampleMap map[string]bool) {
+	smnDb, _ := simple_util.LongFiles2MapArray(paths, "\t", nil)
+
+	// title
+	var title []string
+	for _, cell := range sheet.Row(0).Cells {
+		title = append(title, cell.Value)
+	}
+
+	for _, item := range smnDb {
+		sample := item["SampleID"]
+		if sampleMap[sample] {
+			item["Sample"] = item["SampleID"]
+			item["Copy_Num"] = item["SMN1_ex7_cn"]
+			item["Detect"] = item["SMN1_ex7_cn"]
+			item["Chr"] = "chr5"
+			item["Start"] = "70241892"
+			item["End"] = "70242003"
+			item["Gene"] = "SMN1"
+			item["OMIM_Gene"] = "SMN1"
+			item["SMN1_result"] = item["SMN1_ex7_cn"]
+			if item["SMN1_ex7_cn"] == "0" {
+				item["SMN1_result"] = "Hom"
+				isSMN1 = true
+			}
+			row := sheet.AddRow()
+			for _, key := range title {
+				row.AddCell().SetString(item[key])
+			}
+		}
+	}
+}
+
+func updateDisease(gene string, item, geneDiseaseDbColumn map[string]string, geneDiseaseDb map[string]map[string]string) {
+	// 基因-疾病
+	gDiseaseDb := geneDiseaseDb[gene]
+	for key, value := range geneDiseaseDbColumn {
+		item[value] = gDiseaseDb[key]
+	}
+}
+
+func updateDiseaseMultiGene(geneList string, item, geneDiseaseDbColumn map[string]string, geneDiseaseDb map[string]map[string]string) {
+	genes := strings.Split(geneList, ";")
+	// 基因-疾病
+	for key, value := range geneDiseaseDbColumn {
+		var vals []string
+		for _, gene := range genes {
+			geneDb, ok := geneDiseaseDb[gene]
+			if ok {
+				vals = append(vals, geneDb[key])
+			}
+			//fmt.Println(gene,":",key,":",vals)
+		}
+		if len(vals) > 0 {
+			item[value] = strings.Join(vals, "\n")
+		}
+	}
+}
+
+var isSharp = regexp.MustCompile(`^#`)
+var isBamPath = regexp.MustCompile(`^## Files : (\S+)`)
+
+func loadQC(files string, quality []map[string]string) {
+	file := strings.Split(files, ",")
+	for i, in := range file {
+		report := simple_util.File2Array(in)
+		for _, line := range report {
+			if isSharp.MatchString(line) {
+				if m := isBamPath.FindStringSubmatch(line); m != nil {
+					quality[i]["bamPath"] = m[1]
+				}
+			} else {
+				m := strings.Split(line, "\t")
+				quality[i][strings.TrimSpace(m[0])] = strings.TrimSpace(m[1])
+			}
+		}
+	}
+}
