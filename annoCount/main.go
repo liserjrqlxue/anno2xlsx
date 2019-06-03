bafcf822f357dd1888583e45474e3133de63514b
王垚燊(Yaoshen Wang) <wangyaoshen@genomics.cn>
2019-06-03 12:01:33 +0800

    add -prefix

--- annoCount/main.go
+++ annoCount/main.go
@@ -14,6 +14,11 @@ var (
 		"",
 		"anno file list",
 	)
+	prefix = flag.String(
+		"prefix",
+		"",
+		"out put prefix, default is -list",
+	)
 )
 
 var keyList = []string{
@@ -31,15 +36,23 @@ func main() {
 		flag.Usage()
 		os.Exit(1)
 	}
+	if *prefix == "" {
+		*prefix = *list
+	}
 
+	out, err := os.Open(*prefix + ".tsv")
+	simple_util.CheckErr(err)
+	defer simple_util.DeferClose(out)
+	fmt.Printf("index\tsingleCount\tcumCount\tfile\n")
+	fmt.Fprintf(out, "index\tsingleCount\tcumCount\tfile\n")
 	xlsxList := simple_util.File2Array(*list)
 	var uniqDb = make(map[string]int)
-	var singeleCount []int
+	var singleCount []int
 	var cumCount []int
 
 	for i, fileName := range xlsxList {
 		_, mapArray := simple_util.Sheet2MapArray(fileName, "filter_variants")
-		singeleCount = append(singeleCount, len(mapArray))
+		singleCount = append(singleCount, len(mapArray))
 		for _, item := range mapArray {
 			var keyArray []string
 			for _, key := range keyList {
@@ -48,8 +61,10 @@ func main() {
 			uniqDb[strings.Join(keyArray, "\t")]++
 		}
 		cumCount = append(cumCount, len(uniqDb))
-		fmt.Printf("%d\t%d\t%d\t%s\n", i, singeleCount[i], cumCount[i], fileName)
+		fmt.Printf("%d\t%d\t%d\t%s\n", i, singleCount[i], cumCount[i], fileName)
+		fmt.Fprintf(out, "%d\t%d\t%d\t%s\n", i, singleCount[i], cumCount[i], fileName)
 	}
-	simple_util.Json2rawFile("singelCount.json", singeleCount)
-	simple_util.Json2rawFile("cumCount.json", cumCount)
+	simple_util.Json2rawFile(*prefix+".singleCount.json", singleCount)
+	simple_util.Json2rawFile(*prefix+".cumCount.json", cumCount)
+	simple_util.Json2rawFile(*prefix+".uniqDb.json", uniqDb)
 }
