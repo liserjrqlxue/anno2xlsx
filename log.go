1295be72ec98e6ca738a6bdef90fc42dee137bae
王垚燊(Yaoshen Wang) <wangyaoshen@genomics.cn>
2020-06-08 16:45:10 +0800

    print tier stats to log

--- log.go
+++ log.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"time"
@@ -10,57 +9,57 @@ import (
 )
 
 func logTierStats(stats map[string]int) {
-	fmt.Printf("Total               Count : %7d\n", stats["Total"])
+	log.Printf("Total               Count : %7d\n", stats["Total"])
 	if stats["Total"] == 0 {
 		return
 	}
 	if *trio {
-		fmt.Printf("  noProband         Count : %7d\n", stats["noProband"])
+		log.Printf("  noProband         Count : %7d\n", stats["noProband"])
 
-		fmt.Printf("Denovo              Hit   : %7d\n", stats["Denovo"])
-		fmt.Printf("  Denovo B/LB       Hit   : %7d\n", stats["Denovo B/LB"])
-		fmt.Printf("  Denovo Tier1      Hit   : %7d\n", stats["Denovo Tier1"])
-		fmt.Printf("  Denovo Tier2      Hit   : %7d\n", stats["Denovo Tier2"])
+		log.Printf("Denovo              Hit   : %7d\n", stats["Denovo"])
+		log.Printf("  Denovo B/LB       Hit   : %7d\n", stats["Denovo B/LB"])
+		log.Printf("  Denovo Tier1      Hit   : %7d\n", stats["Denovo Tier1"])
+		log.Printf("  Denovo Tier2      Hit   : %7d\n", stats["Denovo Tier2"])
 	}
 
-	fmt.Printf("ACMG noB/LB         Hit   : %7d\n", stats["noB/LB"])
+	log.Printf("ACMG noB/LB         Hit   : %7d\n", stats["noB/LB"])
 	if *trio {
-		fmt.Printf("  +isDenovo         Hit   : %7d\n", stats["isDenovo noB/LB"])
-		fmt.Printf("    +isAF           Hit   : %7d\n", stats["Denovo AF"])
-		fmt.Printf("      +isGene       Hit   : %7d\n", stats["Denovo Gene"])
-		fmt.Printf("        +isFunction Hit   : %7d\tTier1\n", stats["Denovo Function"])
-		fmt.Printf("        +noFunction Hit   : %7d\n", stats["Denovo noFunction"])
-		fmt.Printf("      +noGene       Hit   : %7d\n", stats["Denovo noGene"])
-		fmt.Printf("    +noAF           Hit   : %7d\n", stats["Denovo noAF"])
-		fmt.Printf("  +noDenovo         Hit   : %7d\n", stats["noDenovo noB/LB"])
-		fmt.Printf("    +isAF           Hit   : %7d\n", stats["noDenovo AF"])
-		fmt.Printf("      +isGene       Hit   : %7d\n", stats["noDenovo Gene"])
-		fmt.Printf("        +isFunction Hit   : %7d\tTier1\n", stats["noDenovo Function"])
-		fmt.Printf("        +noFunction Hit   : %7d\n", stats["noDenovo noFunction"])
-		fmt.Printf("      +noGene       Hit   : %7d\n", stats["noDenovo noGene"])
-		fmt.Printf("    +noAF           Hit   : %7d\n", stats["noDenovo noAF"])
+		log.Printf("  +isDenovo         Hit   : %7d\n", stats["isDenovo noB/LB"])
+		log.Printf("    +isAF           Hit   : %7d\n", stats["Denovo AF"])
+		log.Printf("      +isGene       Hit   : %7d\n", stats["Denovo Gene"])
+		log.Printf("        +isFunction Hit   : %7d\tTier1\n", stats["Denovo Function"])
+		log.Printf("        +noFunction Hit   : %7d\n", stats["Denovo noFunction"])
+		log.Printf("      +noGene       Hit   : %7d\n", stats["Denovo noGene"])
+		log.Printf("    +noAF           Hit   : %7d\n", stats["Denovo noAF"])
+		log.Printf("  +noDenovo         Hit   : %7d\n", stats["noDenovo noB/LB"])
+		log.Printf("    +isAF           Hit   : %7d\n", stats["noDenovo AF"])
+		log.Printf("      +isGene       Hit   : %7d\n", stats["noDenovo Gene"])
+		log.Printf("        +isFunction Hit   : %7d\tTier1\n", stats["noDenovo Function"])
+		log.Printf("        +noFunction Hit   : %7d\n", stats["noDenovo noFunction"])
+		log.Printf("      +noGene       Hit   : %7d\n", stats["noDenovo noGene"])
+		log.Printf("    +noAF           Hit   : %7d\n", stats["noDenovo noAF"])
 	} else {
-		fmt.Printf("    +isAF           Hit   : %7d\n", stats["isAF"])
-		fmt.Printf("      +isGene       Hit   : %7d\n", stats["isGene"])
-		fmt.Printf("        +isFunction Hit   : %7d\tTier1\n", stats["isFunction"])
-		fmt.Printf("        +noFunction Hit   : %7d\n", stats["noFunction"])
-		fmt.Printf("      +noGene       Hit   : %7d\n", stats["noGene"])
-		fmt.Printf("    +noAF           Hit   : %7d\n", stats["noAF"])
+		log.Printf("    +isAF           Hit   : %7d\n", stats["isAF"])
+		log.Printf("      +isGene       Hit   : %7d\n", stats["isGene"])
+		log.Printf("        +isFunction Hit   : %7d\tTier1\n", stats["isFunction"])
+		log.Printf("        +noFunction Hit   : %7d\n", stats["noFunction"])
+		log.Printf("      +noGene       Hit   : %7d\n", stats["noGene"])
+		log.Printf("    +noAF           Hit   : %7d\n", stats["noAF"])
 	}
 
-	fmt.Printf("HGMD/ClinVar        Hit   : %7d\n", stats["HGMD/ClinVar"])
-	fmt.Printf("  isAF              Hit   : %7d\n", stats["HGMD/ClinVar isAF"])
-	fmt.Printf("    noMT            Hit   : %7d\tTier1\n", stats["HGMD/ClinVar noMT T1"])
-	fmt.Printf("  noAF              Hit   : %7d\n", stats["HGMD/ClinVar noAF"])
-	fmt.Printf("    noMT            Hit   : %7d\tTier2\n", stats["HGMD/ClinVar noMT T2"])
+	log.Printf("HGMD/ClinVar        Hit   : %7d\n", stats["HGMD/ClinVar"])
+	log.Printf("  isAF              Hit   : %7d\n", stats["HGMD/ClinVar isAF"])
+	log.Printf("    noMT            Hit   : %7d\tTier1\n", stats["HGMD/ClinVar noMT T1"])
+	log.Printf("  noAF              Hit   : %7d\n", stats["HGMD/ClinVar noAF"])
+	log.Printf("    noMT            Hit   : %7d\tTier2\n", stats["HGMD/ClinVar noMT T2"])
 
-	fmt.Printf("SpecVar             Hit   : %7d\n", stats["SpecVar"])
+	log.Printf("SpecVar             Hit   : %7d\n", stats["SpecVar"])
 
-	fmt.Printf("Retain              Count : %7d\n", stats["Retain"])
-	fmt.Printf("  Tier1             Count : %7d\n", stats["Tier1"])
-	fmt.Printf("    遗传相符        Count : %7d\n", stats["遗传相符"])
-	fmt.Printf("  Tier2             Count : %7d\n", stats["Tier2"])
-	fmt.Printf("  Tier3             Count : %7d\n", stats["Tier3"])
+	log.Printf("Retain              Count : %7d\n", stats["Retain"])
+	log.Printf("  Tier1             Count : %7d\n", stats["Tier1"])
+	log.Printf("    遗传相符        Count : %7d\n", stats["遗传相符"])
+	log.Printf("  Tier2             Count : %7d\n", stats["Tier2"])
+	log.Printf("  Tier3             Count : %7d\n", stats["Tier3"])
 }
 
 func logTime(timeList []time.Time, step1, step2 int, message string) {
