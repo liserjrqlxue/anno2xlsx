1a5bd3c5f936766295ca6d3eca3a4e8ad2f8bda5
王垚燊(Yaoshen Wang) <wangyaoshen@genomics.cn>
2021-02-07 14:15:40 +0800

    :sparkles:add flag to buildHPO

--- buildHPO/main.go
+++ buildHPO/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
+	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/liserjrqlxue/goUtil/osUtil"
@@ -11,26 +14,58 @@ import (
 	"github.com/liserjrqlxue/goUtil/textUtil"
 )
 
-type chpo struct {
+type cHPO struct {
 	HpoID  string `json:"hpo_id"`
 	NameCn string `json:"name_cn"`
 }
 
+// os
+var (
+	ex, _  = os.Executable()
+	exPath = filepath.Dir(ex)
+	dbPath = filepath.Join(exPath, "..", "db")
+)
+
+//flag
+var (
+	output = flag.String(
+		"output",
+		filepath.Join(dbPath, "gene2hpo.txt"),
+		"path to output",
+	)
+	g2p = flag.String(
+		"g2p",
+		filepath.Join(exPath, "genes_to_phenotype.txt"),
+		"path to gene2_to_phenotype.txt",
+	)
+	chpo = flag.String(
+		"chpo",
+		filepath.Join(exPath, "chpo-2021.json"),
+		"path to chpo.json",
+	)
+)
+
 func main() {
-	var chpoArray []chpo
+	flag.Parse()
+
+	var out = osUtil.Create(*output)
+	defer simpleUtil.DeferClose(out)
+
+	var chpoArray []cHPO
+	var getNameCn = make(map[string]string)
+	var gene2hpo = make(map[string]map[string]string)
+
 	simpleUtil.CheckErr(
 		json.Unmarshal(
-			simpleUtil.HandleError(ioutil.ReadFile("chpo-2021.json")).([]byte),
+			simpleUtil.HandleError(ioutil.ReadFile(*chpo)).([]byte),
 			&chpoArray,
 		),
 	)
-	var getNameCn = make(map[string]string)
 	for _, hpo := range chpoArray {
 		getNameCn[hpo.HpoID] = hpo.NameCn
 	}
 
-	var gene2hpo = make(map[string]map[string]string)
-	for _, array := range textUtil.File2Slice("genes_to_phenotype.txt", "\t")[1:] {
+	for _, array := range textUtil.File2Slice(*g2p, "\t")[1:] {
 		var geneID = array[0]
 		var hpoID = array[2]
 		var item, ok = gene2hpo[geneID]
@@ -41,22 +76,9 @@ func main() {
 		gene2hpo[geneID] = item
 	}
 
-	var out = osUtil.Create("gene2chpo.txt")
-	defer simpleUtil.DeferClose(out)
 	simpleUtil.HandleError(
 		fmt.Fprintf(
 			out,
-			"%s\t%s\t%s\n",
-			"entre-gene-id",
-			"HPO-Term-ID",
-			"HPO-Term-NameCN",
-		),
-	)
-	var out2 = osUtil.Create("gene2chpos.txt")
-	defer simpleUtil.DeferClose(out2)
-	simpleUtil.HandleError(
-		fmt.Fprintf(
-			out2,
 			"%s\t%s\t%s\n",
 			"entre-gene-id",
 			"HPO-Term-ID",
@@ -70,19 +92,10 @@ func main() {
 			if nameCn != "" {
 				NameCns = append(NameCns, nameCn)
 			}
-			simpleUtil.HandleError(
-				fmt.Fprintf(
-					out,
-					"%s\t%s\t%s\n",
-					geneID,
-					hpoID,
-					nameCn,
-				),
-			)
 		}
 		simpleUtil.HandleError(
 			fmt.Fprintf(
-				out2,
+				out,
 				"%s\t%s\t%s\n",
 				geneID,
 				strings.Join(hpoIDs, ","),
